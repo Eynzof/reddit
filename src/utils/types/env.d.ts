1cd4bcaeaab9281d68047b96d52e4ea6df73a925
enzo <Eynzof@gmail.com>
2022-10-30 00:35:12 +0800

    🎨(env): update env example

--- src/utils/types/env.d.ts
+++ src/utils/types/env.d.ts
@@ -1,17 +1,13 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
-    STATUS: string;
-    REDIS_URL_DEV: string;
     REDIS_URL_PROD: string;
+    REDIS_URL_DEV: string;
     REDIS_SECRET: string;
-    POSTGRES_PORT: string;
     POSTGRES_USERNAME: string;
     POSTGRES_PASSWORD: string;
     POSTGRES_DATABASE: string;
+    POSTGRES_PORT: string;
     POSTGRES_HOST_PROD: string;
-    POSTGRES_HOST_DEV: string;
-    TOKEN_ACCESS_LIFETIME: string;
-    JWT_SECRET: string;
-    NODE_DEV: string;
+    POSRGRES_HOST_DEV: string;
   }
 }
